dfc7c5611cc956e3bf7f6dcca101a729e0454aa0
Swostika <poudelswostika2@gmail.com>
2024-06-05 21:50:48 +0545

    props

--- components/ListGroup.tsx
+++ components/ListGroup.tsx
@@ -1,19 +1,21 @@
-import { Fragment } from "react";
-function ListGroup() {
-const items =[
-  'New York',
-  'San Francisco',
-  'Tokyo',
-  'London',
-  'Paris'
-];
+import { Fragment, useState } from "react";
+interface Props{
+  items: string[];
+  heading: string;
+onSelectItem: (item: string)=> void;
+}
+function ListGroup({items, heading, onSelectItem}: Props) {
 
-
+ const [selectedIndex, setSelectedIndex] = useState(-1);
   return (<Fragment>
-    <h1>List</h1>
+    <h1>{heading}</h1>
+    {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-     { items.map((item) => 
-     (<li key={item}>{item}</li>
+     { items.map((item, index) => 
+     (<li className = { selectedIndex == index ? 'list-group-item active' : 'list-group-item'} key={item} 
+     onClick={() => { setSelectedIndex(index);
+    onSelectItem(item);
+    } }>{item}</li>
      ))}
       </ul>
   </Fragment>
